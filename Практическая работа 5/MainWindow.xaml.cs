1a8adc01916d382d537d42aac263afbe4ac1f182
исп-31 <-31@LAB30-10>
2021-12-14 13:30:40 +0300

    Добавьте файлы проекта.

---
+++ Практическая работа 5/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Практическая_работа_5
+{
+   
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        Triad plus1 = new Triad();//обьект, содержащий результат увеличения чисел на 1
+        Triad param = new Triad();//обьект, содержащий результат увеличения чисел на 10
+        Triad chisla = new Triad();//объект, содержащий числа
+        Triad sum = new Triad();//объект, содержащий сумму
+
+        private void Сброс_Click(object sender, RoutedEventArgs e)
+        {
+            Ch1.Text = null;
+            Ch2.Text = null;
+            Ch3.Text = null;
+            Plus1.Text = null;          
+        }
+
+        private void Опрограмме_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Потапкин. Задание: Использовать класс Triad (тройка чисел). Разработать операцию инкремента полей на 1.Разработать операцию для получения полной суммы триады с числом");
+        }
+
+        private void Выход_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Plus1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                chisla.One = Convert.ToInt32(Ch1.Text);//присваиваем значение из текст бокса
+                chisla.Second = Convert.ToInt32(Ch2.Text);//присваиваем значение из текст бокса
+                chisla.Third = Convert.ToInt32(Ch3.Text);//присваиваем значение из текст бокса
+                chisla++;//используем оператор
+                Plus1.Text = chisla.One.ToString() + ";" + chisla.Second.ToString() + ";" + chisla.Third.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("ОШИБКА");
+            }
+            }
+
+        //private void Plus10_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        chisla.One = Convert.ToInt32(Ch1.Text);//присваиваем значение из текст бокса
+        //        chisla.Second = Convert.ToInt32(Ch2.Text);//присваиваем значение из текст бокса
+        //        chisla.Third = Convert.ToInt32(Ch3.Text);//присваиваем значение из текст бокса
+        //        param.SetParams(chisla.One, chisla.Second, chisla.Third,out string rez1);//используем функцию
+        //        Plus10.Text = rez1;//записывем результат вычислений в текст бокс
+        //    }
+        //    catch
+        //    {
+        //        MessageBox.Show("ОШИБКА");
+        //    }
+
+        //}
+
+        
+
+        private void Sum_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                chisla.One = Convert.ToInt32(Ch1.Text);//присваиваем значение из текст бокса
+                chisla.Second = Convert.ToInt32(Ch2.Text);//присваиваем значение из текст бокса
+                chisla.Third = Convert.ToInt32(Ch3.Text);//присваиваем значение из текст бокса
+                int val = Convert.ToInt32(Ch4.Text);//присваиваем значение из текст бокса
+                sum = chisla + val;//используем оператор
+                Sum.Text = sum.One.ToString();//записывем результат вычислений в текст бокс
+            }
+            catch
+            {
+                MessageBox.Show("ОШИБКА");
+            }
+        }
+    }
+}
---
+++ Практическая работа 5/Triad.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа_5
+{
+    class Triad
+    {
+        int one;//первое число
+        int second;//второе число
+        int third;//третье число
+        public int One //Первое число
+        {
+            get
+            {
+                return one;
+            }
+            set
+            {
+                one = value;
+
+            }
+        }
+
+        public int Second //Второе число
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                second = value;
+
+            }
+        }
+        public int Third //Третье число
+        {
+            get
+            {
+                return third;
+            }
+            set
+            {
+                third = value;
+
+            }
+        }
+        public Triad Plus1(int One, int Second, int Third)
+        {
+            Triad plus = new Triad();
+
+            {
+                plus.One = One + 1;
+                plus.Second = Second + 1;
+                plus.Third = Third + 1;
+
+            }
+            return plus;
+        }
+
+        public static Triad operator ++(Triad c1)
+        {
+            Triad rezult = new Triad();
+            rezult.One = c1.One + 1;
+            rezult.Second = c1.Second + 1;
+            rezult.Third = c1.Third + 1;
+            return rezult;
+        }
+        public static Triad operator +(Triad c1, int val)
+        {
+            Triad rezult = new Triad();
+            rezult.One = c1.One + c1.Second + c1.Third + val;
+            return rezult;
+        }
+    }
+}
+//        public bool SetParams()
+//        {
+//            {
+//                One = One + 10;
+//                Second = Second + 10;
+//                Third = Third + 10;
+//                return true;
+//            }
+//        }
+//        public void SetParams(int One, int Second, int Third, out string _rez1)
+//        {
+//            int _One = 0;
+//            int _Second = 0;
+//            int _Third = 0;
+//            _rez1 = null;
+
+//            if (One != 0)
+//            {
+//                _One = One + 10;
+//            }
+//            else _One = 0;
+//            if (Second != 0)
+//            {
+//                _Second = Second + 10;
+//            }
+//            else _Second = 0;
+//            if (Third != 0)
+//            {
+//                _Third = Third + 10;
+//            }
+//            else _Third = 0;
+//            _rez1 = _One.ToString() + ";" + _Second.ToString() + ";" + _Third.ToString();
+
+//        }
+//    }
+//}
+
